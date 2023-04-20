29792881b9088337bfc4d3928f9e4ac27ea0da89
Maksim Gurkov <maksim.gurkov@gmail.com>
2023-04-20 09:03:31 +0300

    Initial Commit

---
+++ TrafficlightUI/TrafficlightUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrafficlightUIApp.swift
+//  TrafficlightUI
+//
+//  Created by Максим Гурков on 20.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct TrafficlightUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
